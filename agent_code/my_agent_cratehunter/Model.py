2425423c73bfe7d65996df5546a30d94272810a2
Taylorsuperfan <127939891+Taylorsuperfan@users.noreply.github.com>
2023-09-14 10:27:13 +0200

    Add files via upload

--- agent_code/my_agent_cratehunter/Model.py
+++ agent_code/my_agent_cratehunter/Model.py
@@ -16,15 +16,15 @@ class DQN(nn.Module):
 
     def __init__(self, channel_in, channel_out):
         super(DQN, self).__init__()
-        '''self.conv0 = nn.Conv2d(in_channels=channel_in, out_channels= 16, kernel_size=2, stride=1)
+        self.conv0 = nn.Conv2d(in_channels=channel_in, out_channels= 16, kernel_size=1, stride=1)
         self.relu0 = nn.ReLU()
-        self.conv1 = nn.Conv2d(in_channels=16, out_channels= 64, kernel_size=2, stride=2)
+        self.conv1 = nn.Conv2d(in_channels=16, out_channels= 64, kernel_size=1, stride=2)
         self.relu1 = nn.ReLU()
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels= 64, kernel_size=2, stride=1)
-        self.relu2 = nn.ReLU()'''
-        self.linear0 = nn.Linear(in_features=channel_in, out_features=32)
-        self.linear1 = nn.Linear(in_features=32, out_features= 64)
-        self.linear2 = nn.Linear(in_features=64, out_features=channel_out)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels= 64, kernel_size=1, stride=1)
+        self.relu2 = nn.ReLU()
+        self.linear0 = nn.Linear(in_features=128, out_features=64)
+        self.linear1 = nn.Linear(in_features=64, out_features= 32)
+        self.linear2 = nn.Linear(in_features=32, out_features=channel_out)
 
         self.relu = nn.ReLU()
 
@@ -33,25 +33,38 @@ class DQN(nn.Module):
 
     def forward(self, x):
         x = state_to_features(x)
-        x = T.tensor(state_to_features(x)).float()
-        '''x = self.conv0(x)
+        x = self.conv0(x)
         x = self.relu0(x)
         x = self.conv1(x)
         x = self.relu1(x)
         x = self.conv2(x)
         x = self.relu2(x)
-        x = torch.flatten(x, 1)'''
+        x = torch.flatten(x, 1)
         x = self.relu(self.linear0(x))
         x = self.relu(self.linear1(x))
         x = self.relu(self.linear2(x))
         return x
     def train_step(self, old_state, action, new_state, reward):
         if action is not None:
-            state_action_value = self.forward(old_state).unsqueeze(0)
+            # action_mask = T.zeros(len(ACTIONS), dtype=T.int64)
+            # action_mask[ACTIONS.index(action)] = 1
+
+            state_action_value = self.forward(old_state)
+
+            # next_state_action_value = self.forward(new_state).max().unsqueeze(0)
+            # expected_state_action_value = (next_state_action_value * self.gamma) + reward
             target = T.tensor(ACTIONS.index(act_rule(old_state)), dtype=T.long).unsqueeze(0)
+            # print(f"target: {target}")
+            # print(f"state_action_value: {state_action_value}")
             loss = self.loss(state_action_value, target)
+            # loss = self.loss(state_action_value.to(self.device), expected_state_action_value.to(self.device))
             with open("loss_log.txt", "a") as loss_log:
                 loss_log.write(str(loss.item()) + "\t")
             self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
+
+
+class Double_Q_Net(nn.Module):
+    def __init__(self, featuren_in, output):
+        super(Double_Q_Net, self).__init__()
--- agent_code/my_agent_cratehunter/stateTofeatures.py
+++ agent_code/my_agent_cratehunter/stateTofeatures.py
@@ -19,61 +19,40 @@ def state_to_features(game_state: dict) -> np.array:
 
     if game_state is None:
         return None
-# Here is the original value of countdown and position, to avoid the None type in feature.
-    position_b = [-1, -1]
-    count_down = [-1]
+    count_down = -1
     # For example, you could construct several channels of equal shape, ...
     _, _, bombholder, (x_self, y_self) = game_state["self"]
-    steps = game_state["step"]
     feature_matrix_shape = game_state["field"].shape
     walls = game_state["field"]
     explosion = np.zeros(feature_matrix_shape)
 
 
 
-
     #coin_map =  np.zeros(feature_matrix_shape)
-    if 0 in bombholder :
-        for i in range(0, len(game_state["bombs"])):
-            for (x, y), c in game_state["bombs"]:
-                count_down = c
-                position_b = (x, y)
-                walls[x, y] = -50
-        # if count_down in range(0, 4):
-        #     for (x, y), c in game_state["bombs"]:
-        #         for i in range(1, 4):
-        #             if walls[x-i, y] == -1:
-        #                 break
-        #             walls[x-i, y] = -25-30/(c+2)
-        #         for i in range(1, 4):
-        #             if walls[x, y-i] == -1:
-        #                 break
-        #             walls[x, y-i] = -25-30/(c+2)
-        #         for i in range(1, 4):
-        #             if walls[x, y+i] == -1:
-        #                 break
-        #             walls[x, y+i] = -25-30/(c+2)
-        #         for i in range(1, 4):
-        #             if walls[x+i, y] == -1:
-        #                 break
-        #             walls[x+i, y] = -25-30/(c+2)
-##Here, we used a list of dx, dy tuples to represent the four possible directions: left (-1, 0), right (1, 0), up (0, -1) and down (0, 1 ).
-#
-# Then, we use a for loop to iterate through the directions and take up to three steps in each direction (range(1, 4)), just like you did in your original code.
-#
-# In this way, we have replaced the original four with two nested for loops and eliminated duplicate code.
+    if bombholder == 0:
+        for (x, y), c in game_state["bombs"]:
+            count_down = c
+            walls[x, y] = -50
         if count_down in range(0, 4):
             for (x, y), c in game_state["bombs"]:
-                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    for i in range(1, 4):
-                        new_x, new_y = x + dx * i, y + dy * i
-                        if walls[new_x, new_y] == -1:
-                            break
-                        walls[new_x, new_y] = -25 - 30 / (c + 2)
+                for i in range(1, 4):
+                    if walls[x-i, y] == -1:
+                        break
+                    walls[x-i, y] = -25-30/(c+2)
+                for i in range(1, 4):
+                    if walls[x, y-i] == -1:
+                        break
+                    walls[x, y-i] = -25-30/(c+2)
+                for i in range(1, 4):
+                    if walls[x, y+i] == -1:
+                        break
+                    walls[x, y+i] = -25-30/(c+2)
+                for i in range(1, 4):
+                    if walls[x+i, y] == -1:
+                        break
+                    walls[x+i, y] = -25-30/(c+2)
     for (x, y) in game_state["coins"]:
         walls[x, y] = 100
-    for _, _, _,(x, y) in game_state["others"]:
-        walls[x, y] = 75
     print(count_down)
     field_matrix = np.copy(walls)
     up_situation = field_matrix[x_self, y_self-1]
@@ -83,18 +62,15 @@ def state_to_features(game_state: dict) -> np.array:
     my_situation = field_matrix[x_self, y_self]
 
 
-    game_feature = np.array(([up_situation, down_situation, left_situation, right_situation]))
-    bomb_feature = np.append(position_b, count_down)
-    bomb_feature = np.append(bomb_feature, my_situation)
-    game_feature = np.vstack((game_feature, bomb_feature))
-
-
-    T.tensor(game_feature)
-
-
-
+    game_feature = np.array(([up_situation, down_situation, left_situation, right_situation, my_situation, count_down]))
+    game_feature = T.tensor(game_feature).view(3, -1).unsqueeze(0).unsqueeze(0).float()
+    print(game_state["step"])
+    print(game_feature)
+    print(walls)
 
     return game_feature
+
+
 
 
 
